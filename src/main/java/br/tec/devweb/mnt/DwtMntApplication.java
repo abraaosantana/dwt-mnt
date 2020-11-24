1b06c8d229f45de6539c41f3721c17d32c987e68
Abraão Santana <deploys.app@gmail.com>
2020-11-23 21:02:06 -0300

    Commit Inicial do projeto e dependencias necessárias

---
+++ src/main/java/br/tec/devweb/mnt/DwtMntApplication.java
@@ -0,0 +1,13 @@
+package br.tec.devweb.mnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwtMntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwtMntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/tec/devweb/mnt/DwtMntApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.devweb.mnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwtMntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
